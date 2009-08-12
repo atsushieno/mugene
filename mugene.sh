61575215955790c8351a82aec3ffa82e460d95a6
Atsushi Eno <atsushieno@MBW.local>
2009-08-12 19:16:24 +0900

    add hacky runner shell script.

---
+++ mugene.sh
@@ -0,0 +1,4 @@
+#MUGENE_INST_DIR=/svn/commons-music-prog/mugene
+DIR=$MUGENE_INST_DIR
+
+mono --debug $MONO_OPTIONS $DIR/bin/Debug/mugene.exe $DIR/mml/default-macro.mml $DIR/mml/gs-sysex.mml $DIR/mml/drum-part.mml $DIR/mml/nrpn-gs-xg.mml $@
