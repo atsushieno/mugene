a422006398f656c0f714515ef300850bdc36f380
atsushi <atsushi@PC.(none)>
2009-08-19 22:12:23 +0900

    removed old shell script. Add some NRPN support in vsq.

--- mugene.sh
+++
@@ -1,4 +0,0 @@
-MUGENE_INST_DIR=/svn/commons-music-prog/mugene
-DIR=$MUGENE_INST_DIR
-
-mono --debug $MONO_OPTIONS $DIR/bin/Debug/mugene.exe $DIR/mml/default-macro.mml $DIR/mml/gs-sysex.mml $DIR/mml/drum-part.mml $DIR/mml/nrpn-gs-xg.mml $@
