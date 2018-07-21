a3dea2c43e0aa457de1f6ead4fd3da30f48ae2ce
Atsushi Eno <atsushieno@gmail.com>
2018-07-21 20:32:39 +0900

    [LSP] remove "copy of template" parts.

--- vscode-extension/client/src/extension.ts
+++ vscode-extension/client/src/extension.ts
@@ -30,17 +30,17 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{scheme: 'file', language: 'plaintext'}],
+		documentSelector: [{scheme: 'file', language: 'mugene'}],
 		synchronize: {
 			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: 'lspSample',
+			configurationSection: 'mugeneLSP',
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
 	
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('lspSample', 'Language Server Example', serverOptions, clientOptions).start();
+	let disposable = new LanguageClient('mugeneLSP', 'mugene Language Server', serverOptions, clientOptions).start();
 	
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
